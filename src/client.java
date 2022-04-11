3ddf93b771bc4ffd704429bc292023c1ebad79a9
manan Patel <manan883@gmail.com>
2022-04-11 16:53:30 -0700

    first commit

---
+++ src/client.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class client {
+protected int[] heap;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	void readFileSetArr() {
+		
+	}
+	public static void sequentialInsert() {
+		
+	}
+	
+	//
+
+}
